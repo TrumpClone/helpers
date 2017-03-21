17559cea96eb8e5aab99c8ef9be567417a87d6cd
exclusive <exclusive@zerweb.com>
2017-03-21 10:21:44 +0300

    adopt TiteiKo's PR'

--- lib/hanami/helpers/form_helper/form_builder.rb
+++ lib/hanami/helpers/form_helper/form_builder.rb
@@ -785,7 +785,7 @@ module Hanami
         #   #  </select>
         def select(name, values, attributes = {}) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
           options    = attributes.delete(:options) { {} }
-          attributes = { name: _input_name(name), id: _input_id(name) }.merge(attributes)
+          attributes = { name: _select_input_name(name, attributes[:multiple]), id: _input_id(name) }.merge(attributes)
           prompt     = options.delete(:prompt)
           selected   = options.delete(:selected)
 
@@ -793,7 +793,7 @@ module Hanami
             option(prompt) unless prompt.nil?
 
             values.each do |content, value|
-              if selected == value || _value(name) == value
+              if _select_option_selected?(value, selected, _value(name), attributes[:multiple])
                 option(content, { value: value, selected: SELECTED }.merge(options))
               else
                 option(content, { value: value }.merge(options))
@@ -1041,6 +1041,17 @@ module Hanami
           !input_value.nil? &&
             (input_value.to_s == value.to_s || input_value.is_a?(TrueClass) ||
             input_value.is_a?(Array) && input_value.include?(value))
+        end
+
+        def _select_input_name(name, multiple)
+          select_name = _input_name(name)
+          select_name = "#{select_name}[]" if multiple
+          select_name
+        end
+
+        def _select_option_selected?(value, selected, input_value, multiple)
+          value == selected || (multiple && (selected.is_a?(Array) && selected.include?(value))) ||
+            value == input_value || (multiple && (input_value.is_a?(Array) && input_value.include?(value)))
         end
       end
     end
